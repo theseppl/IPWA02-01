d427e7ff28ade5b34dd878112e23e4e2ddc58f0f
Sebastian Stark <sstark@gmx.net>
2025-07-08 20:58:57 +0200

    Zweiter Push

--- TestProjekt/src/TestClass.java
+++ TestProjekt/src/TestClass.java
@@ -2,7 +2,7 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, GIT es gut?");
+		System.out.println("Hello World, GIT es gut heute?");
 
 	}
 
