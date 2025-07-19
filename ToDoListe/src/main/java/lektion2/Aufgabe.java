d6d94d951696a0dca0672bf0e68f05d689accec9
Sebastian Stark <sstark@gmx.net>
2025-07-19 09:26:19 +0200

    Java Klassen Aufgabe und Aufgabenliste angepasst

--- ToDoListe/src/main/java/lektion2/Aufgabe.java
+++ ToDoListe/src/main/java/lektion2/Aufgabe.java
@@ -1,12 +1,26 @@
 package lektion2;
 
+import java.util.Date;
+
 public class Aufgabe {
 	private String text;
 	private String person;
+	Date faelligAm;
+	Boolean erledigt;
+	float aufwand;
 
 	public Aufgabe(String text, String person) {
 		this.text = text;
 		this.person = person;
+	}
+	
+	public Aufgabe(String text, String person, Date faelligAm, Boolean erledigt, float aufwand) {
+		super();
+		this.text = text;
+		this.person = person;
+		this.faelligAm = faelligAm;
+		this.erledigt = erledigt;
+		this.aufwand = aufwand;
 	}
 
 	public String getPerson() {
@@ -24,4 +38,30 @@ public class Aufgabe {
 	public void setText(String text) {
 		this.text = text;
 	}
+
+	public Date getFaelligAm() {
+		return faelligAm;
+	}
+
+	public void setFaelligAm(Date faelligAm) {
+		this.faelligAm = faelligAm;
+	}
+
+	public Boolean getErledigt() {
+		return erledigt;
+	}
+
+	public void setErledigt(Boolean erledigt) {
+		this.erledigt = erledigt;
+	}
+
+	public float getAufwand() {
+		return aufwand;
+	}
+
+	public void setAufwand(float aufwand) {
+		this.aufwand = aufwand;
+	}
+	
+	
 }
--- ToDoListe/src/main/java/lektion2/Aufgabenliste.java
+++ ToDoListe/src/main/java/lektion2/Aufgabenliste.java
@@ -1,6 +1,8 @@
 package lektion2;
 
 import java.io.Serializable;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +18,17 @@ public class Aufgabenliste implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private static Aufgabenliste instance = new Aufgabenliste();
 	private List<Aufgabe> liste = new ArrayList<Aufgabe>();
+	
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
 
 	public Aufgabenliste() {
-		liste.add(new Aufgabe("tue dies", "Jakob"));
-		liste.add(new Aufgabe("tue das", "Nina"));
-		liste.add(new Aufgabe("tue jenes", "Jessie"));
+		try {
+			liste.add(new Aufgabe("tue dies", "Jane", dateFormat.parse("04.05.1984"), true, 14));
+			liste.add(new Aufgabe("tue das", "Basti", dateFormat.parse("09.04.1981"), false, 8));
+			liste.add(new Aufgabe("tue jenes", "Nelie", dateFormat.parse("13.06.2007"), false, 22));
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public static Aufgabenliste getInstance() {
